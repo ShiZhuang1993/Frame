4c2e5889c93641c3a34b3f9c6e26cfcfae7e0f84
史壮壮 <zhuangzhuang.shi@kangso.net>
2018-08-10 10:47:08 +0800

    初始化

--- app/src/main/java/com/frame/frame/base/BaseFragment.java
+++ app/src/main/java/com/frame/frame/base/BaseFragment.java
@@ -14,28 +14,43 @@ import android.view.ViewGroup;
  * author:zhuang
  * ===================================
  */
-public abstract class BaseFragment extends Fragment implements View.OnClickListener{
+public abstract class BaseFragment extends Fragment implements View.OnClickListener {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(setView(), container, false);
+        View inflate = inflater.inflate(setView(), container, false);
+        init(inflate);
+        return inflate;
     }
-    //布局文件
-    protected abstract int setView();
-    //控件ID
-    protected abstract void init(View view);
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData(savedInstanceState);
+        initListener();
+    }
+
     //初始化数据
     protected abstract void initData(Bundle savedInstanceState);
+
     // 添加监听器
     protected abstract void initListener();
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 
     @Override
     public void onPause() {
         super.onPause();
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
+    //布局文件
+    protected abstract int setView();
+
+    //控件ID
+    protected abstract void init(View view);
+
+
 }
